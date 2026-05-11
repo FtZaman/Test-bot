b23c758ec4a598a0296439c21be1b1928ac31561
乛 𝐓ⴙ𝛆̽ 𝐙𝛂ϻ֯𝛂ƞ̽ 🎧✨ <luciferxd971@gmail.com>
2026-05-11 22:12:26 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const {
+  default: makeWASocket,
+  useMultiFileAuthState
+} = require("@whiskeysockets/baileys")
+
+const pino = require("pino")
+
+async function startBot() {
+
+  const { state, saveCreds } =
+    await useMultiFileAuthState("session")
+
+  const sock = makeWASocket({
+    auth: state,
+    logger: pino({ level: "silent" })
+  })
+
+  sock.ev.on("creds.update", saveCreds)
+
+  sock.ev.on("messages.upsert", async ({ messages }) => {
+
+    const msg = messages[0]
+    if (!msg.message) return
+
+    const text =
+      msg.message.conversation || ""
+
+    if (text === ".ping") {
+
+      await sock.sendMessage(
+        msg.key.remoteJid,
+        { text: "Pong ✅" }
+      )
+    }
+  })
+}
+
+startBot()
